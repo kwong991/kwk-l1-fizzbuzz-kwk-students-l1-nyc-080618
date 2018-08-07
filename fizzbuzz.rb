5a07b0fb2f9718cef87bf59cf7e0343252c5901e
kwong991 <kgwong991@gmail.com>
2018-08-07 19:42:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,3 +4,10 @@ describe "fizzbuzz" do
  
     expect(fizz_3).to eq("Fizz")
   end
+
+describe "fizzbuzz" do
+  it 'returns "Buzz" when the number is divisible by 5' do
+    fizz_5 = fizzbuzz(5)
+end
+
+
