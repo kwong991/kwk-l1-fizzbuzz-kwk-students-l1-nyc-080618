2d96e726c0d8ba8c46714438921a57b60b8589f7
kwong991 <kgwong991@gmail.com>
2018-08-07 20:05:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,19 @@
-fizzbuzz = 4
- "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
+# fizzbuzz = 4
+# "fizzbuzz" do
+#   it 'returns "Fizz" when the number is divisible by 3' do
+#     fizz_3 = fizzbuzz(3)
  
-    expect(fizz_3).to eq("Fizz")
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
+#     expect(fizz_3).to eq("Fizz")
+#   end
+#   it 'returns "Buzz" when the number is divisible by 5' do
+#     fizz_5 = fizzbuzz(5)
  
-    expect(fizz_5).to eq("Buzz")
-  end
-  it 'returns "FizzBuzz" when the number is divisible by 15' do
-    fizz_15 = fizzbuzz(15)
+#     expect(fizz_5).to eq("Buzz")
+#   end
+#   it 'returns "FizzBuzz" when the number is divisible by 15' do
+#     fizz_15 = fizzbuzz(15)
  
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-end
+#     expect(fizz_15).to eq("FizzBuzz")
+#   end
+# end
+def 
